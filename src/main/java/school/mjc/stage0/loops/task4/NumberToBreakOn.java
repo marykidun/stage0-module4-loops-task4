966cbcd7aa5d92178f3bccae253329a1e424c6f7
Maryna Kidun <marykidun@gmail.com>
2022-11-03 00:21:21 +0200

    Finished 4 tasks

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,24 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
+        for (int i = 0; i <= numberToGoUntil; i++) {
+            if (numberToGoUntil < toBreakWith) {
+                System.out.println("iterating till the end");
+                System.out.println(i);
+                break;
+            }
+                for (int j = 0; j <= toBreakWith; j++) {
+                    if (numberToGoUntil > toBreakWith) {
+                        System.out.println(j);
+                    }
+                }
+            }
+        }
+
+
+    public static void main(String[] args) {
+        printNumbersUntilWithBreakOn(5,4);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,26 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+    public static void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        int number = numberToSkip + lastInRow;
 
+        for (int i = 0; i <= 1; i++) {
+            if (numberToSkip > lastInRow && lastInRow > 0) {
+                System.out.println("number to skip is bigger then the last");
+                break;
+            }
+                else if  (lastInRow < 0) {
+                System.out.println("last number in row is negative");
+                break;
+            }
+                else
+                System.out.println("skipped sum is number");
+                System.out.println("counted sum is number " + number);
+
+            }
+        }
+
+    public static void main(String [] arg) {
+        printSumOfTwoRanges(100, 112);
     }
 }
